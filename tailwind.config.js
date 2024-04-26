d8421c7431552c16e1fbae5edce2471ad9faceab
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-04-26 22:57:49 +0530

    toggleTheme functionality created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        white: "#ffffff",
+        black: "#000000",
+      },
+    },
   },
   plugins: [],
 };
+
+
+
 
